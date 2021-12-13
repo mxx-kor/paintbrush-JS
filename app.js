38836ad6e921e491824af2c3fd22bf94e9d73805
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2021-12-14 01:32:00 +0900

    2D Context

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.lineWidth = 2.5;
+ctx.strokeStyle = "#2c2c2c"
 
 let painting = false;
 
@@ -6,13 +13,20 @@ function stopPainting() {
     painting = false;
 }
 
+function startPainting() {
+    painting = true;
+}
+
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-}
-
-function onMouseDown(event) {
-    painting = true;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function onMouseUp(event) {
@@ -22,7 +36,7 @@ function onMouseUp(event) {
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
