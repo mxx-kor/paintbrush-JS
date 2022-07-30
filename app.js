8b59fde45370221bdf4267889f6ba55496b71e4e
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-07-30 17:59:30 +0900

    added two new features
    사진을 불러와 그림에 사용할 수 있고
    캔버스에 더블클릭하여 텍스트를 추가하는 기능을 구현

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mode = document.getElementById("jsMode");
 const resetBtn = document.getElementById('reset_btn');
 const eraseBtn = document.getElementById('erase_btn');
 const saveBtn = document.getElementById("jsSave");
+const fileInput = document.getElementById("file");
+const textInput = document.getElementById("text");
 
 const INITIAL_COLOR = "#2c2c2c"
 const CANVAS_SIZE = 800;
@@ -16,6 +18,7 @@ canvas.height = CANVAS_SIZE;
 ctx.fillStyle = "white"
 ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.lineWidth = 2.5;
+ctx.lineCap = "round";
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 
@@ -103,39 +106,51 @@ function onColorClick(event) {
     color.value = colorValue;
 }
 
-if (canvas) {
-    canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", startPainting);
-    canvas.addEventListener("mouseup", stopPainting);
-    canvas.addEventListener("mouseleave", stopPainting);
-    canvas.addEventListener("click", handleCanvasClick);
-    canvas.addEventListener("contextmenu", handleCM);
+function onFileChange(event) {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src = url;
+    image.onload = function () {
+        ctx.drawImage(image, 0, 0, CANVAS_SIZE, CANVAS_SIZE)
+    };
+    fileInput.value = null;
 }
+
+function onDoubleClick(event) {
+    const text = textInput.value;
+    if (text !== "") {
+        ctx.save();
+        ctx.lineWidth = 1;
+        ctx.font = "48px serif";
+        ctx.fillText(text, event.offsetX, event.offsetY);
+        ctx.restore();
+    }
+}
+
+canvas.addEventListener("dblclick", onDoubleClick);
+canvas.addEventListener("mousemove", onMouseMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", stopPainting);
+canvas.addEventListener("mouseleave", stopPainting);
+canvas.addEventListener("click", handleCanvasClick);
+canvas.addEventListener("contextmenu", handleCM);
 
 colorOptions.forEach(color => color.addEventListener("click", onColorClick))
 
-if(range) {
-    range.addEventListener("change", handleRangeChange);
-}
 
-if(mode) {
-    mode.addEventListener("click", handleModeClick);
-}
+range.addEventListener("change", handleRangeChange);
 
-if(saveBtn) {
-    saveBtn.addEventListener("click", handleSaveClick);
-}
+mode.addEventListener("click", handleModeClick);
 
-if(resetBtn) {
-    resetBtn.addEventListener('click', onResetClick);
-}
+saveBtn.addEventListener("click", handleSaveClick);
 
-if(eraseBtn) {
-    eraseBtn.addEventListener('click', onEraseClick);
-}
+resetBtn.addEventListener('click', onResetClick);
 
-if(color){
-    color.addEventListener('change', onColorChange);
-}
+eraseBtn.addEventListener('click', onEraseClick);
 
-console.dir(color)
+color.addEventListener('change', onColorChange);
+
+fileInput.addEventListener("change", onFileChange)
+
+
